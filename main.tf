ff500d14d620a28400686e41dd8541296d06b89e
implodingduck <zsek320@gmail.com>
2021-06-07 08:09:45 -0500

    changing cosmosdb free tier attribute

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
-  enable_free_tier    = true
+  enable_free_tier    = false
   consistency_policy {
     consistency_level       = "Session"
   }
