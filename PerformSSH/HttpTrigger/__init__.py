0728177c104013179adef77665905d5b9ee2d90e
implodingduck <zsek320@gmail.com>
2021-07-22 10:45:07 -0500

    stuff

--- PerformSSH/HttpTrigger/__init__.py
+++ PerformSSH/HttpTrigger/__init__.py
@@ -6,7 +6,7 @@ import paramiko
 import os
 import datetime
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python PerformSSH HTTP trigger function processed a request.')
 
     name = req.params.get('name')
     if not name:
