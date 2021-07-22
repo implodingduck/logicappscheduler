b7098ac5a23c3b59cbb06b4c61923158a0085026
implodingduck <zsek320@gmail.com>
2021-07-22 12:15:28 -0500

    lets try adding a vm

--- main.tf
+++ main.tf
@@ -192,3 +192,41 @@ resource "azurerm_public_ip" "pip" {
 data "template_file" "nginx-vm-cloud-init" {
   template = file("${path.module}/install-nginx.sh")
 }
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${local.ssh_func_name}-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${local.ssh_func_name}-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  admin_password      = random_password.password.result
+  custom_data    = base64encode(data.template_file.nginx-vm-cloud-init.rendered)
+  
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
