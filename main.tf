3a8793d46dd45d56384b2a503253209ec2c8a902
implodingduck <zsek320@gmail.com>
2021-07-27 09:30:07 -0500

    adding nsg to vm

--- main.tf
+++ main.tf
@@ -197,6 +197,7 @@ resource "azurerm_network_interface" "nic" {
   name                = "${local.ssh_func_name}-vm-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
     name                          = "internal"
